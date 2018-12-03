93c0a7a4613cf85abfd79c79b3cf3c227a1ef842
manu <manu@windows7-insti>
2018-12-03 14:16:45 +0100

    Biblioteca

--- Biblioteca/src/servlets/Biblioteca.java
+++ Biblioteca/src/servlets/Biblioteca.java
@@ -2,7 +2,6 @@ package servlets;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -22,16 +21,23 @@ public class Biblioteca extends HttpServlet {
         super();
     }
     
-    protected void leeFicheroLibros() throws FileNotFoundException {
-    	File ficheroLibros = new File("/ficheros/libros.txt");
+    ArrayList<String> libros = new ArrayList<String>();
+    
+    public void leeFicheroLibros() throws IOException {
+    	String libro;
+    	
+    	File ficheroLibros = new File("ficheros/libros.txt");
     	FileReader fr = new FileReader(ficheroLibros);
     	BufferedReader br = new BufferedReader(fr);
     	
-    	ArrayList<String> libros = new ArrayList<String>();
-    	
+    	while((libro = br.readLine()) != null) {
+            libros.add(libro);
+        }
+    	br.close();
     }
-    
+
     protected void procesaSolicitud(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 
     	response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
     	
@@ -39,22 +45,38 @@ public class Biblioteca extends HttpServlet {
 	    esAjax="XMLHttpRequest".equalsIgnoreCase(request.getHeader("X-Requested-With")); // Cabecera X-Requested-With
 	    if (esAjax) {
 	        //Recoge el parámetro isbn
-	        String usuario=request.getParameter("isbn");
-	        Boolean isbnValido=false;
+	        String isbn = request.getParameter("isbn");
+	        boolean isbnValido = false;
+	        //String[] libroSeleccionado = {""};
+	        String libro = "";
 	        
-	        if (isbnValido==false) {
-	            System.out.println("Usuario no válido");
-	        }
+        	for(int i=0; i<libros.size(); i++) {
+        		String[] datosLibro = libros.get(i).split(";");
+        		if(datosLibro[0].equals(isbn)) {
+        			isbnValido = true;
+        			libro = datosLibro[1];
+        			break;
+        		}
+        	}
+        	if(isbnValido == false) {
+        		out.println("ISBN no válido.");
+        	} else {
+        		/*out.println("ISBN: " + libroSeleccionado[0]);
+        		out.println("Título: " + libroSeleccionado[1]);
+        		out.println("Autor: " + libroSeleccionado[2]);
+        		out.println("Año: " + libroSeleccionado[3]);*/
+        		out.println("ISBN válido.");
+        	}
 	    } else {
 	        out.println("Este servlet solo se puede invocar vía Ajax");
 	    }
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		procesaSolicitud(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		procesaSolicitud(request, response);
 	}
 }
